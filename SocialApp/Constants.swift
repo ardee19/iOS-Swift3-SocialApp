f88f3b6c278ad5e97f3017e5b2648ff9adf7b066
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2016-11-09 01:46:32 -0800

    Add Sign-In UI Custom Classes

---
+++ SocialApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialApp
+//
+//  Created by Roy Dimayuga on 11/9/16.
+//  Copyright © 2016 Super Awesome. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY:CGFloat = 120.0 / 255.0
