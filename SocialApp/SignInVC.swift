e6165a7ef86e08996e15d280cd8be2201dc9f218
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2016-11-09 17:06:37 -0800

    Implement Facebook Sign-in

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,33 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookButtonTapped(_ sender: UIButton) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) {
+             (result, error) in
+            
+            if error != nil {
+                print("ROY: Unable to Authenticate with facebook - \(error.debugDescription)")
+            } else if result?.isCancelled == true {
+                print("ROY: User Cancelled Facebook Authentication")
+            } else {
+                print("ROY Successfully Authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuthenticate(credential)
+            }
+        }
+    }
+    
+    func firebaseAuthenticate(_ credential:FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("ROY: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("ROY: Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
