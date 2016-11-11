7a440067b2b174bd95c007c30887c0bcf0d81efe
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2016-11-10 18:00:45 -0800

    Complete Feed UI

--- SocialApp/RoundImageView.swift
+++ SocialApp/RoundImageView.swift
@@ -1,15 +1,15 @@
 //
-//  CustomView.swift
+//  RoundImageView.swift
 //  SocialApp
 //
-//  Created by Roy Dimayuga on 11/8/16.
+//  Created by Roy Dimayuga on 11/10/16.
 //  Copyright © 2016 Super Awesome. All rights reserved.
 //
 
 import UIKit
 
-class CustomView: UIView {
-
+class RoundImageView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -17,8 +17,20 @@ class CustomView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        
-        layer.cornerRadius = 2.0
-        
     }
+
+    /*
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+    */
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
 }
